90223e4811bae11b4e1bcc950b8207f1c4c5a0fa
riditalove <fabihaafrose69@gmail.com>
2022-05-14 01:03:56 +0600

    Modified sample_branch_b1 form B2

--- app/src/main/java/Branch/Sample_Branch_B1.java
+++ app/src/main/java/Branch/Sample_Branch_B1.java
@@ -3,12 +3,23 @@ package Branch;
 public class Sample_Branch_B1 {
     String name,id ;
     float cgpa ;
-    Sample_Branch_B1(String s,String s1,float f)
-    {
-        this.name = s;
-        this.id = s1;
-        this.cgpa = f;
+    double income;
+
+    public Sample_Branch_B1(String name, String id, float cgpa, double income) {
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+        this.income = income;
     }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
     public String getName()
     {
         return  name;
